1521c962768f0eea7b58cb3fd00ce6660a50f2e7
Jaya Mukesh <61919375+fishcupboard@users.noreply.github.com>
2021-03-21 15:18:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,14 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Pothittu Po!"
+    return str(s)
+
+s=11
+
+def main():
+    s=10
+    return None
+
+
+if __name__ == '__main__':
+    main()
